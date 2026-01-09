152135137e5882f40000c72b34cc69628478b7bc
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-09 13:28:52 +0100

    set up some basic syntax showcase

--- src/main/java/com/stocktcg/Main.java
+++ src/main/java/com/stocktcg/Main.java
@@ -1,8 +1,21 @@
 package com.stocktcg;
 
-public class Main {
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Stock TCG");
+        stage.show(); 
+    }    
     public static void main(String[] args) {
-        System.out.println("Welcome to Stock TCG!");
+        launch(args);
     }
     
 }
